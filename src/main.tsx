7481bfc0e9f5c6622e85e5eed0ea709a8be122ec
Miguel Espinoza <migueles@microsoft.com>
2022-11-11 16:43:07 -0800

    add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,22 @@ import App from "./App";
 import "./index.css";
 import "./domain/handleOpenFile";
 import ReactModal from "react-modal";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 const element = document.getElementById("root") as HTMLElement;
+
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_URL,
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
 ReactDOM.createRoot(element).render(
   <React.StrictMode>
     <App />
